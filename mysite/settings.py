fcfd7821cdf9e5e491878244b4f53433325dbef1
Vahini <vmadipalli@unomaha.edu>
2022-05-05 23:06:03 -0500

    google api updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'shop',
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
